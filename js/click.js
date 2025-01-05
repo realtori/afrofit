005fbedf4e95f26c2010dbc3a01e04c73ccf66e2
realtori <44826025+realtori@users.noreply.github.com>
2025-01-05 13:15:05 +1300

    Add files via upload

---
+++ js/click.js
@@ -0,0 +1,21 @@
+let left = document.querySelector('.left')
+let right = document.querySelector('.right')
+let butt = document.querySelector('.butt ul')
+let buttLi = document.querySelectorAll('.butt ul li')
+let currentIndex = 0;
+let buttLiwidth = buttLi[0].clientWidth + 5
+const buttLilength = buttLi.length -2;
+
+function updateCarousel() {
+             butt.style.transform = `translateX(-${currentIndex * buttLiwidth}px)`;
+}
+
+left.addEventListener('click', function() {
+            currentIndex = (currentIndex > 0) ? currentIndex - 1 : buttLilength - 1;
+            updateCarousel();
+});
+right.addEventListener('click', function() {
+            currentIndex = (currentIndex < buttLilength - 1) ? currentIndex + 1 : 0;
+            updateCarousel();
+});
+
